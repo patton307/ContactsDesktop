b838d07672576cf1f4d0875b117589b89cd810e8
landonkail <landon.kail@gmail.com>
2015-10-20 15:58:19 -0400

    initial commit

---
+++ src/sample/Contact.java
@@ -0,0 +1,24 @@
+package sample;
+
+/**
+ * Created by landonkail on 10/20/15.
+ */
+public class Contact {
+    String name;
+    String phone;
+    String email;
+
+    public Contact(String name, String phone, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %s", name, phone, email);
+    }
+}
+
+
+
---
+++ src/sample/Controller.java
@@ -0,0 +1,54 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    ObservableList<Contact> contacts = FXCollections.observableArrayList();
+
+    @FXML
+    ListView listView;
+
+    @FXML
+    TextField textName;
+
+    @FXML
+    TextField textPhone;
+
+    @FXML
+    TextField textEmail;
+
+    public void createContact () {
+        do {
+            Contact contact = new Contact(textName.getText(), textPhone.getText(), textEmail.getText());
+            contacts.add(contact);
+        } while (textName.getText().equals("") || textPhone.getText().equals("") || textEmail.getText().equals(""));
+
+
+        textName.setText("");
+        textPhone.setText("");
+        textEmail.setText("");
+    }
+
+    public void removeContact () {
+        Contact contact = (Contact) listView.getSelectionModel().getSelectedItem();
+        if (contact != null ) {
+            contacts.remove(contact);
+        }
+    }
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        listView.setItems(contacts);
+    }
+}
